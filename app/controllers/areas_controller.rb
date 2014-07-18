ee8129d80661657c8910e90b61afe3cd4bfeea12
Raghu Betina <git@rugb.com>
2014-07-18 08:58:26 -0500

    Initial commit

---
+++ app/controllers/areas_controller.rb
@@ -0,0 +1,42 @@
+class AreasController < ApplicationController
+  def square
+    @a = params[:length_of_side].to_f
+    @area = @a**2
+  end
+
+  def circle
+    pi = 3.1416 # Close enough.
+    @r = params[:radius].to_f
+    @area = pi * @r**2
+  end
+
+  def triangle
+    @b = params[:base].to_f
+    @h = params[:vertical_height].to_f
+    @area = 0.5 * @b * @h
+  end
+
+  def rectangle
+    @w = params[:width].to_f
+    @h = params[:height].to_f
+    @area = @w * @h
+  end
+
+  def trapezoid
+    @a = params[:a].to_f
+    @b = params[:b].to_f
+    @h = params[:vertical_height].to_f
+    @area = 0.5 * (@a + @b) * @h
+  end
+
+  def ellipse
+    pi = 3.1416 # Close enough.
+    @a = params[:a].to_f
+    @b = params[:b].to_f
+    @area = pi * @a * @b
+  end
+
+  def random
+    @radii = [rand(1..20), rand(1..20), rand(1..20), rand(1..20), rand(1..20)]
+  end
+end
